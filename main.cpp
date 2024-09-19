599a7e231ae9d24af8246ce300ef58d59d6c0915
BIG-KG <bek.georgy@gmail.com>
2024-09-20 02:29:23 +0300

    deleta_codestile
    откат по кодстайлу по sorting.cpp тк прога перестала запускаться :[

--- main.cpp
+++ main.cpp
@@ -5,11 +5,13 @@ int main(){
     char *allStr = NULL;
     string_start_end *stringsArray = NULL;
     int in_file = open("input.txt", _O_BINARY | O_RDONLY );
-
+    printf("yy");
     int numStrings = enter_text_struct(&stringsArray, in_file, &allStr);
+    printf("iii");
 	//quick_sort(stringsArray, numStrings, sizeof(string_start_end), struct_compare_strings);
 	quick_sort(stringsArray, numStrings, sizeof(string_start_end),
-               struct_compare_strings_rev);
+               struct_compare_strings);
+    printf("uuu");
     print_struct_strings(stringsArray, numStrings);
     close(in_file);
     free(allStr);
